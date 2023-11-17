25f084cc8d821e713124e1093088b5e913907cab
Serban681 <serbands20@gmail.com>
2023-11-17 18:07:07 +0200

    added fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
+import FooterComponent from "@/components/GeneralComponents/Footer";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <FooterComponent />
+    </>
+  );
 };
 
 export default MyApp;
