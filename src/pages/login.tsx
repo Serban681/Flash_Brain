b3a80775aef7cfcc2a48a1d3c9ed2d4be03060e2
DanceaRadu <radu.dancea@gmail.com>
2023-11-17 18:09:22 +0200

    First commit d

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,17 @@
+import styles from '@/styles/login-page.module.css';
 export default function LoginPage() {
+
     return (
-        <>Login</>
+        <div className={styles.loginPageOuterDiv}>
+            <form className={styles.loginForm}>
+                <p className={styles.loginP}>Log in</p>
+                <button className={styles.googleButton}>Sign-up with Google</button>
+                <div className={styles.loginLine}>
+                    <div></div>
+                    <p>or</p>
+                    <div></div>
+                </div>
+            </form>
+        </div>
     )
 }
