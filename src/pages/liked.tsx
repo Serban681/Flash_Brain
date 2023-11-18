0cfdfcfc12a5d0fb83480d02758d8bc40dac2335
DanceaRadu <radu.dancea@gmail.com>
2023-11-18 19:08:31 +0200

    Added liked page. 2

--- src/pages/liked.tsx
+++ src/pages/liked.tsx
@@ -1,11 +1,23 @@
 import Header from "@/components/GeneralComponents/Header";
 import styles from "@/styles/liked.module.css";
+import useCheckLoggedIn from "@/utils/useCheckLoggedIn";
+import {useEffect} from "react";
+import router from "next/router";
 
 export default function liked() {
+
+    const {isLoggedIn, isPending: isPendingLoggedIn, userInformation} = useCheckLoggedIn();
+
+    useEffect(() => {
+        if(!isPendingLoggedIn && !isLoggedIn) router.push("/login");
+    }, [isLoggedIn, isPendingLoggedIn]);
+
     return (
         <div className={styles.likedPageOuterDiv}>
             <Header></Header>
-            <div className={styles.likedContentDiv}></div>
+            <div className={styles.likedContentDiv}>
+                <p className={styles.title}>Liked summaries</p>
+            </div>
         </div>
     )
 }
