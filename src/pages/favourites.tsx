b26fb900f1d23b3f2c829a254f8bb605bfd25ee5
Serban681 <serbands20@gmail.com>
2023-11-19 02:28:20 +0200

    Starte working on liked summaries

--- src/pages/favourites.tsx
+++ src/pages/favourites.tsx
@@ -3,13 +3,13 @@ import styles from "@/styles/liked.module.css";
 import useCheckLoggedIn from "@/utils/useCheckLoggedIn";
 import {useEffect} from "react";
 import router from "next/router";
-import useFetchLikedSummaries from "@/utils/useFetchLikedSummaries";
-import useFetchSummaries from "@/utils/useFetchSummaries";
 import useFetchUserSummaries from "@/utils/useFetchUserSummaries";
+import SummaryCard from "@/components/MainPageComponents/SummaryCard";
 
 export default function FavouritesPage() {
 
     const {isLoggedIn, isPending: isPendingLoggedIn, userInformation} = useCheckLoggedIn();
+
     let {
         error,
         isPending,
@@ -24,11 +24,61 @@ export default function FavouritesPage() {
         if(!isPendingLoggedIn && !isLoggedIn) router.push("/login");
     }, [isLoggedIn, isPendingLoggedIn]);
 
+    function getSummarySecondaryColor(index: number) {
+        switch (index % 6) {
+            case 0:
+                return 'var(--light-green)'
+            case 1:
+                return 'var(--light-black)'
+            case 2:
+                return 'var(--light-yellow)'
+            case 3:
+                return 'var(--light-yellow)'
+            case 4:
+                return 'var(--light-green)'
+            case 5:
+                return 'var(--light-black)'
+        }
+    }
+
+    function getSummaryBackground(index: number) {
+        switch (index % 6) {
+            case 0:
+                return 'var(--green)'
+            case 1:
+                return 'var(--black)'
+            case 2:
+                return 'var(--yellow)'
+            case 3:
+                return 'var(--yellow)'
+            case 4:
+                return 'var(--green)'
+            case 5:
+                return 'var(--black)'
+        }
+    }
+
     return (
         <div className={styles.likedPageOuterDiv}>
             <Header></Header>
-            <div className={styles.likedContentDiv}>
-                <h2 className="float-right">Liked Summaries</h2>
+            <div className={`w-screen ${styles.likedContentDiv}`}>
+                <h2 className="">Liked Summaries</h2>
+                {summaryList.length > 0 && <div className={styles.summaryBrowser}>
+                    {summaryList.map((summary, index) => (
+                        <div key={summary.summaryId} style={{borderRadius:10}}>
+                            <SummaryCard
+                                summary={summary}
+                                backgroundColor={getSummaryBackground(index)}
+                                secondaryColor={getSummarySecondaryColor(index)}
+                            ></SummaryCard>    
+                        </div>
+                    ))}
+                </div>}
+                {summaryList.length == 0 && !isPending && <p style={{
+                              fontFamily:'var(--font-josefin)',
+                              fontSize: 26,
+                              color: 'var(--white)'
+                          }}>No results found</p>}
             </div>
         </div>
     )
