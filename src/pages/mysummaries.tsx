6a7f36a961994952cc778d81ebe86455f9225703
DanceaRadu <radu.dancea@gmail.com>
2023-12-23 19:22:49 +0200

    Added option to delete your own generated summaries.

--- src/pages/mysummaries.tsx
+++ src/pages/mysummaries.tsx
@@ -2,17 +2,18 @@ import Header from "@/components/GeneralComponents/Header";
 import styles from "@/styles/index.module.css";
 import likeStyles from "@/styles/liked.module.css";
 import useCheckLoggedIn from "@/utils/useCheckLoggedIn";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import router from "next/router";
 import SummaryCard from "@/components/MainPageComponents/SummaryCard";
+import delete_icon from "@/images/delete_icon.svg"
+import Image from "next/image";
+import config from "@/config";
 
 export default function MySummariesPage() {
 
     const {isLoggedIn, isPending: isPendingLoggedIn, userInformation} = useCheckLoggedIn(0);
-
-    useEffect(() => {
-        console.log(userInformation?.summaries);
-    }, [userInformation?.summaries]);
+    const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+    const [deletedSummaryId, setDeletedSummaryId] = useState<number>(-1);
 
     useEffect(() => {
         if(!isPendingLoggedIn && !isLoggedIn) router.push("/login");
@@ -52,6 +53,34 @@ export default function MySummariesPage() {
         }
     }
 
+    function handleDeleteSummary(summaryId: number) {
+        if(!userInformation?.summaries) return;
+        const index = userInformation.summaries.findIndex(summary => summary.summaryId == summaryId);
+        if (index === -1) {
+            return;
+        }
+
+        setDeletedSummaryId(summaryId);
+        setIsLoadingDelete(true);
+        fetch(config.apiUrl + "/summary/" + summaryId,
+            {method: 'DELETE',
+                headers: {
+                    "Origin":config.origin,
+                    "Authorization": "Bearer " + localStorage.getItem('jwtToken')}}
+        )
+            .then(res => {
+                if(!res.ok) throw Error("Couldn't delete summary");
+                setIsLoadingDelete(false);
+                if(userInformation?.summaries) userInformation.summaries.splice(index, 1);
+                setDeletedSummaryId(-1);
+            })
+            .catch(() => {
+                setIsLoadingDelete(false);
+                setDeletedSummaryId(-1);
+            })
+
+    }
+
     return (
         <div className={likeStyles.likedPageOuterDiv}>
             <Header></Header>
@@ -64,12 +93,19 @@ export default function MySummariesPage() {
                         <h2 className={`${likeStyles.title} mb-8`}>My Summaries</h2>
                         {userInformation?.summaries && userInformation.summaries.length > 0 && <div className={styles.summaryBrowser}>
                             {userInformation.summaries.map((summary, index) => (
-                                <div key={summary.summaryId} style={{borderRadius:10}}>
+                                <div className="flex flex-col" key={summary.summaryId} style={{borderRadius:10}}>
                                     <SummaryCard
                                         summary={summary}
                                         backgroundColor={getSummaryBackground(index)}
                                         secondaryColor={getSummarySecondaryColor(index)}
                                     ></SummaryCard>
+                                    <button
+                                        onClick={() => handleDeleteSummary(summary.summaryId)}
+                                        className="delete-btn">
+                                        {isLoadingDelete && deletedSummaryId === summary.summaryId && <div className="lds-dual-ring" style={{scale:"0.35"}}></div>}
+                                        {(!isLoadingDelete || deletedSummaryId !== summary.summaryId) && <Image className="relative bottom-0.5 mr-1" src={delete_icon} alt="delete"/>}
+                                        {(!isLoadingDelete ||  deletedSummaryId !== summary.summaryId) && "Delete"}
+                                    </button>
                                 </div>
                             ))}
                         </div>}
